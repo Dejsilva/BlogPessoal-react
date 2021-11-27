2abd202800bdb105a9476263536d762e54a0b234
Dejsilva <dejs_silva@hotmail.com>
2021-11-27 15:01:19 -0300

    React primeiro contato

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return(
+        <>
+            <h1 className = "titulo">Home</h1>
+            <img src="http://pm1.narvii.com/7828/849557d07a9390fc03b546f24fca611a7ac51640r1-736-670v2_uhq.jpg" alt="Goku Blue" />
+        </>
+     );
+}
+
+export default Home;
