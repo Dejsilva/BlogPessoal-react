d8bcb81bd921a74c1cd91b4e3e556e674ce3568b
Dejsilva <dejs_silva@hotmail.com>
2021-11-30 16:05:04 -0300

    Menu, Rodape, home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Navbar from './components/estaticos/navbar/Navbar';
 import './App.css';
+import Footer from "./components/estaticos/footer/Footer";
+import {Grid} from "@material-ui/core";
 import Home from './paginas/home/Home';
 
 
 function App() {
   return (
-  <Home />
+  <>
+    <Navbar />
+    <Home />
+    <Footer />
+  </>
   );
 }
 
