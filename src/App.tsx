985ccfae797b871d8e86cf7f71b6b71066941be2
Dejsilva <dejs_silva@hotmail.com>
2021-12-01 16:57:03 -0300

    Inserindo Cadastrar

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,38 @@ import React from 'react';
 import Navbar from './components/estaticos/navbar/Navbar';
 import './App.css';
 import Footer from "./components/estaticos/footer/Footer";
-import {Grid} from "@material-ui/core";
+import CadastroUsuario from './paginas/login/cadastroUsuario/CadastroUsuario'
 import Home from './paginas/home/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './paginas/login/Login';
 
 
 function App() {
   return (
-  <>
-    <Navbar />
-    <Home />
-    <Footer />
-  </>
+    <Router>
+      <Navbar />
+
+      <Switch>
+        <div style={{ minHeight: '100vh' }}>
+
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/home">
+            <Home />
+          </Route>
+
+          <Route path="/cadastrousuario">
+            <CadastroUsuario />
+          </Route>
+        </div>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
